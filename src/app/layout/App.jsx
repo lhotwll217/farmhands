ba29e7e0f24ba7f09d299412e1241f7e07eb85d9
lhotwll217 <lukehenryotwell@gmail.com>
2021-10-16 14:00:02 -0400

    NavBar

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,7 +1,12 @@
+import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar";
+
 function App() {
   return (
-    <div className='App'>
+    <div>
+      <NavBar />
       <h1>FarmHands</h1>
+      <EventDashboard />
     </div>
   );
 }
---
+++ src/features/nav/NavBar.jsx
@@ -0,0 +1,27 @@
+import { Button, Container, Menu, MenuItem } from "semantic-ui-react";
+
+export default function NavBar() {
+  return (
+    <Menu inverted fixed='top'>
+      <Container>
+        <MenuItem>
+          <img src='/assets/logo.png' alt='logo'></img>
+          Re-vents
+        </MenuItem>
+        <MenuItem name='Events' />
+        <MenuItem>
+          <Button positive inverted content='Create Event' />
+        </MenuItem>
+        <MenuItem>
+          <Button basic inverted content='Login' />
+          <Button
+            basic
+            inverted
+            content='Register'
+            style={{ marginLeft: ".5em" }}
+          />
+        </MenuItem>
+      </Container>
+    </Menu>
+  );
+}
