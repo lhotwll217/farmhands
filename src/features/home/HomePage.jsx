8c1e0156aeb3e5f39b481dd0e5f327bfc90ddb2a
lhotwll217 <lukehenryotwell@gmail.com>
2021-10-17 17:55:43 -0400

    Landing Page

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -1,3 +1,30 @@
-export default function HomePage() {
-  return <h1> HomePage</h1>;
+import { useHistory } from "react-router";
+import {
+  Icon,
+  Button,
+  Container,
+  Header,
+  Image,
+  Segment,
+} from "semantic-ui-react";
+
+export default function HomePage({ history }) {
+  return (
+    <Segment inverted textAlign='center' className='masthead'>
+      <Container>
+        <Header as='h1' inverted>
+          <Image
+            size='massive'
+            src='/assets/logo.png'
+            style={{ marginBottom: 12 }}
+          />
+          Re-vents
+        </Header>
+        <Button onClick={() => history.push("/events")} size='huge' inverted>
+          Get Started
+          <Icon name='right arrow' />
+        </Button>
+      </Container>
+    </Segment>
+  );
 }
