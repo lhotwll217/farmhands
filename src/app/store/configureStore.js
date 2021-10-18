86cfb8fe6d887d55030eed983cb30e98fcf4f9b3
lhotwll217 <lukehenryotwell@gmail.com>
2021-10-18 12:37:55 -0400

    Timetravel Debugging

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,6 +1,7 @@
 import { createStore } from "redux";
 import testReducer from "../../features/sandbox/testReducer";
+import { devToolsEnhancer } from "redux-devtools-extension";
 
 export function configureStore() {
-  return createStore(testReducer);
+  return createStore(testReducer, devToolsEnhancer());
 }
