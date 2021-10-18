a216fc01fb82c8d39fddf233b558da51daca49b1
lhotwll217 <lukehenryotwell@gmail.com>
2021-10-18 12:20:48 -0400

    Switched Reducer

--- src/features/sandbox/Sandbox.jsx
+++ src/features/sandbox/Sandbox.jsx
@@ -1,11 +1,25 @@
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import { Button } from "semantic-ui-react";
+import { DECREMENT_COUNTER, INCREMENT_COUNTER } from "./testReducer";
 
 export default function Sandbox() {
   const data = useSelector((state) => state.data);
+  const dispatch = useDispatch();
   return (
     <>
       <h1> Testing 123</h1>
       <h3>The data is: {data} </h3>
+      <Button
+        onClick={() => dispatch({ type: INCREMENT_COUNTER })}
+        content='Increment'
+        color='green'
+      />
+      <Button
+        onClick={() => dispatch({ type: DECREMENT_COUNTER })}
+        content='Decrement'
+        color='red'
+      />
     </>
   );
 }
