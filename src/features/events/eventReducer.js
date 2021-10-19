8c360991c05ffa6ff458f63b90e68fca211c32bf
lhotwll217 <lukehenryotwell@gmail.com>
2021-10-19 10:07:14 -0400

    Redux Event Delete Debug

--- src/features/events/eventReducer.js
+++ src/features/events/eventReducer.js
@@ -23,7 +23,7 @@ export default function eventReducer(state = initialState, { type, payload }) {
     case DELETE_EVENT:
       return {
         ...state,
-        events: [...state.events.filter((evt) => evt.id !== payload.id)],
+        events: [...state.events.filter((evt) => evt.id !== payload)],
       };
     default:
       return state;
