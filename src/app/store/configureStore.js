eb8ed5ca5547fb93d1eef6d6f85ccb523da38f2a
lhotwll217 <lukehenryotwell@gmail.com>
2021-10-21 18:55:20 -0400

    Async Redux

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,8 +1,8 @@
-import { createStore } from "redux";
-
-import { devToolsEnhancer } from "redux-devtools-extension";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./rootReducer";
 
 export function configureStore() {
-  return createStore(rootReducer, devToolsEnhancer());
+  return createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 }
