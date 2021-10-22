7401596ecee8a492588afef95e973d6d1e7e2c2c
lhotwll217 <lukehenryotwell@gmail.com>
2021-10-21 22:13:46 -0400

    Event Loading Placeholders

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -2,16 +2,22 @@ import { useSelector } from "react-redux";
 import { Grid, GridColumn } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import EventList from "./EventList";
+import EventListItemPlaceholder from "./EventListPlaceholder";
 
 export default function EventDashboard() {
   const { events } = useSelector((state) => state.event);
   const { loading } = useSelector((state) => state.async);
 
-  if (loading) return <LoadingComponent />;
-
   return (
     <Grid>
       <GridColumn width={10}>
+        {loading && (
+          <>
+            <EventListItemPlaceholder />
+            <EventListItemPlaceholder />
+            <EventListItemPlaceholder />
+          </>
+        )}
         <EventList events={events} />
       </GridColumn>
       <GridColumn width={6}>
