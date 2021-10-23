2743a639f1b2fe939ac38a2936f248a75e848b81
lhotwll217 <lukehenryotwell@gmail.com>
2021-10-22 21:56:06 -0400

    Firestore setup & listen

---
+++ src/app/config/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/database";
+import "firebase/auth";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyACqkWDy55u2YFE-StIUMMsRBDWUxkk6Kg",
+  authDomain: "farmhandscourse.firebaseapp.com",
+  projectId: "farmhandscourse",
+  storageBucket: "farmhandscourse.appspot.com",
+  messagingSenderId: "984062980230",
+  appId: "1:984062980230:web:891166ce6389129241a107",
+  measurementId: "G-2L4VF0RJ90",
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+export default firebase
--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,13 +1,23 @@
 import { useSelector } from "react-redux";
 import { Grid, GridColumn } from "semantic-ui-react";
 import EventFilters from "./EventFilters";
-
+import { useEffect } from "react";
 import EventList from "./EventList";
 import EventListItemPlaceholder from "./EventListPlaceholder";
+import { getEventsFromFirestore } from "../../../app/firestore/firestoreService";
 
 export default function EventDashboard() {
   const { events } = useSelector((state) => state.event);
   const { loading } = useSelector((state) => state.async);
+
+  useEffect(() => {
+    const unsubscribe = getEventsFromFirestore({
+      next: (snapshot) =>
+        console.log(snapshot.docs.map((docSnapshot) => docSnapshot.data())),
+      error: (error) => console.log(error),
+    });
+    return unsubscribe;
+  });
 
   return (
     <Grid>
