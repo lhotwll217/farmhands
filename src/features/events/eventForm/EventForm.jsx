47b5eb8d0ee164cf2ddf60ffc10b8b3ae335ade6
lhotwll217 <lukehenryotwell@gmail.com>
2021-10-24 15:42:12 -0400

    firestoreService Create/ Update

--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -1,10 +1,10 @@
 /* global google */
 import { Header, Segment, Button } from "semantic-ui-react";
 import cuid from "cuid";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import { createEvent, updateEvent } from "../eventActions";
+import { createEvent, listenToEvents, updateEvent } from "../eventActions";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
 import MyTextInput from "../../../app/common/form/MyTextInput";
@@ -13,12 +13,22 @@ import MySelectInput from "../../../app/common/form/MySelectInput.jsx";
 import MyDateInput from "../../../app/common/form/MyDateInput.jsx";
 import { categoryOptions } from "../../../app/common/form/categoryOptions.js";
 import MyPlaceInput from "../../../app/common/form/MyPlaceInput";
+import useFirestoreDoc from "../../../app/hooks/useFirstoreDoc";
+import {
+  addEventToFirestore,
+  listenToEventFromFirestore,
+  updateEventInFirestore,
+} from "../../../app/firestore/firestoreService";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { toast } from "react-toastify";
 
 export default function EventForm({ match, history }) {
   const dispatch = useDispatch();
   const selectedEvent = useSelector((state) =>
     state.event.events.find((e) => e.id === match.params.id)
   );
+
+  const { loading, error } = useSelector((state) => state.async);
 
   const validationSchema = Yup.object({
     title: Yup.string().required("You must provide a title"),
@@ -33,6 +43,12 @@ export default function EventForm({ match, history }) {
     date: Yup.string().required("You must provide a date"),
   });
 
+  useFirestoreDoc({
+    query: () => listenToEventFromFirestore(match.params.id),
+    data: (event) => dispatch(listenToEvents([event])),
+    deps: [match.params.id],
+  });
+
   const initialValues = selectedEvent ?? {
     title: "",
     category: "",
@@ -42,24 +58,27 @@ export default function EventForm({ match, history }) {
     date: "",
   };
 
+  if (loading || (!selectedEvent && !error))
+    return <LoadingComponent content='Loading event ...' />;
+
+  if (error) return <Redirect to='/error' />;
+
   return (
     <Segment clearing>
       <Header content={selectedEvent ? "Edit The Event" : "Create New Event"} />
       <Formik
         initialValues={initialValues}
-        onSubmit={(values) => {
-          selectedEvent
-            ? dispatch(updateEvent({ ...selectedEvent, ...values }))
-            : dispatch(
-                createEvent({
-                  ...values,
-                  id: cuid(),
-                  hostedBy: "Bob",
-                  attendees: [],
-                  hostPhotoURL: "/assets/user.png",
-                })
-              );
-          history.push("/events");
+        onSubmit={async (values, { setSubmitting }) => {
+          try {
+            selectedEvent
+              ? await updateEventInFirestore(values)
+              : await addEventToFirestore(values);
+            setSubmitting(false);
+            history.push("/events");
+          } catch (error) {
+            toast.error(error.message);
+            setSubmitting(false);
+          }
         }}
         validationSchema={validationSchema}
       >
