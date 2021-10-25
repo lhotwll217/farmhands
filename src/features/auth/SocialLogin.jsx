7f443f2d06d7c235c29930f9bc853831a5724d68
lhotwll217 <lukehenryotwell@gmail.com>
2021-10-25 15:22:17 -0400

    Facebook Sign-In

--- src/features/auth/SocialLogin.jsx
+++ src/features/auth/SocialLogin.jsx
@@ -1,10 +1,21 @@
+import { useDispatch } from "react-redux";
 import { Button } from "semantic-ui-react";
+import { closeModal } from "../../app/common/form/modals/modalReducer";
+import { socialLogin } from "../../app/firestore/firebaseService";
 
 export default function SocialLogin() {
+  const dispatch = useDispatch();
+
+  function handleSocialLogin(provider) {
+    dispatch(closeModal());
+    socialLogin(provider);
+  }
+
   return (
     <>
       <Button
         icon='facebook'
+        onClick={() => handleSocialLogin("facebook")}
         fluid
         color='facebook'
         style={{ marginBottom: 10 }}
@@ -12,6 +23,7 @@ export default function SocialLogin() {
       />
       <Button
         icon='google'
+        onClick={() => handleSocialLogin("google")}
         fluid
         color='google plus'
         content='Login with Google'
