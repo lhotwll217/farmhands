7f6ddd1742bd5f087635bc1dfd98f1ca001fc825
lhotwll217 <lukehenryotwell@gmail.com>
2021-10-26 19:42:18 -0400

    Image Cropper

--- src/app/common/photos/PhotoUploadWidget.jsx
+++ src/app/common/photos/PhotoUploadWidget.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
-import { Grid, GridColumn, Header } from "semantic-ui-react";
+import {
+  Button,
+  ButtonGroup,
+  Grid,
+  GridColumn,
+  Header,
+} from "semantic-ui-react";
+import PhotoWidgetCropper from "./PhotoWidgetCropper";
 import PhotoWidgetDropzone from "./PhotoWidgetDropzone";
 
 export default function PhotoUploadWidget() {
   const [files, setFiles] = useState([]);
+  const [image, setImage] = useState(null);
   return (
     <Grid>
       <GridColumn width={4}>
@@ -13,10 +21,28 @@ export default function PhotoUploadWidget() {
       <GridColumn width={1} />
       <GridColumn width={4}>
         <Header color='teal' sub content='Step 2 - Resize' />
+        {files.length > 0 && (
+          <PhotoWidgetCropper
+            setImage={setImage}
+            imagePreview={files[0].preview}
+          />
+        )}
       </GridColumn>
       <GridColumn width={1} />
       <GridColumn width={4}>
         <Header color='teal' sub content='Step 3 - Preview & Upload' />
+        {files.length > 0 && (
+          <>
+            <div
+              className='img-preview'
+              style={{ minHeight: 200, minWidth: 200, overflow: "hidden" }}
+            />{" "}
+            <ButtonGroup>
+              <Button style={{ width: 100 }} positive icon='check' />
+              <Button style={{ width: 100 }} icon='close' />
+            </ButtonGroup>
+          </>
+        )}
       </GridColumn>
       <GridColumn width={1} />
     </Grid>
