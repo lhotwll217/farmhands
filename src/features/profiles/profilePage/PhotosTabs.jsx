88e33ceebc750c076ece04ade92447966f0ee329
lhotwll217 <lukehenryotwell@gmail.com>
2021-10-27 12:41:40 -0400

    Delete Photo From Firestore & Firebase

--- src/features/profiles/profilePage/PhotosTabs.jsx
+++ src/features/profiles/profilePage/PhotosTabs.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
 import {
   Button,
   CardGroup,
@@ -13,7 +14,12 @@ import {
   ButtonGroup,
 } from "semantic-ui-react";
 import PhotoUploadWidget from "../../../app/common/photos/PhotoUploadWidget";
-import { getUserPhotos } from "../../../app/firestore/firestoreService";
+import { deleteFromFirebaseStorage } from "../../../app/firestore/firebaseService";
+import {
+  deletePhotoFromCollection,
+  getUserPhotos,
+  setMainPhoto,
+} from "../../../app/firestore/firestoreService";
 import useFirestoreCollection from "../../../app/hooks/useFirestoreCollection";
 import { listenToUserPhotos } from "../profileActions";
 
@@ -22,12 +28,37 @@ export default function PhotosTab({ profile, isCurrentUser }) {
   const { loading } = useSelector((state) => state.async);
   const { photos } = useSelector((state) => state.profile);
   const dispatch = useDispatch();
+  const [updating, setUpdating] = useState({ isUpdating: false, target: null });
+  const [deleting, setDeleting] = useState({ isDeleting: false, target: null });
 
   useFirestoreCollection({
     query: () => getUserPhotos(profile.id),
     data: (photos) => dispatch(listenToUserPhotos(photos)),
     deps: [profile.id, dispatch],
   });
+
+  async function handleSetMainPhoto(photo, target) {
+    setUpdating({ isUpdating: true, target });
+    try {
+      await setMainPhoto(photo);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setUpdating({ isUpdating: false, target: null });
+    }
+  }
+
+  async function handleDeletePhoto(photo, target) {
+    setDeleting({ isDeleting: true, target });
+    try {
+      await deleteFromFirebaseStorage(photo.name);
+      await deletePhotoFromCollection(photo.id);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setDeleting({ isDeleting: false, target: null });
+    }
+  }
 
   return (
     <TabPane loading={loading}>
@@ -56,8 +87,28 @@ export default function PhotosTab({ profile, isCurrentUser }) {
                 <Card key={photo.id}>
                   <Image src={photo.url} />
                   <ButtonGroup fluid widths={2}>
-                    <Button basic color='green' content='Main' />
-                    <Button basic color='red' icon='trash' />
+                    <Button
+                      name={photo.id}
+                      loading={
+                        updating.isUpdating && updating.target === photo.id
+                      }
+                      onClick={(e) => handleSetMainPhoto(photo, e.target.name)}
+                      basic
+                      color='green'
+                      content='Main'
+                      disabled={photo.url === profile.photoURL}
+                    />
+                    <Button
+                      name={photo.id}
+                      onClick={(e) => handleDeletePhoto(photo, e.target.name)}
+                      basic
+                      color='red'
+                      icon='trash'
+                      loading={
+                        deleting.isDeleting && deleting.target === photo.id
+                      }
+                      disabled={photo.url === profile.photoURL}
+                    />
                   </ButtonGroup>
                 </Card>
               ))}
